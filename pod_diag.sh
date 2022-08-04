ad084056b460b3a665f574c0c6362fe284d0839c
nogodan <choi.taeho@gmail.com>
2022-08-04 12:26:19 +1000

    Update pod_diag.sh

--- pod_diag.sh
+++ pod_diag.sh
@@ -5,9 +5,8 @@
 
 kubectl get namespace -o custom-columns=NAMESPACE:.metadata.name --no-headers | while read namespace
 do
-         kubectl get pods -n $namespace --field-selector=status.phase!=Running --no-headers  -o custom-columns=pod_name:.metadata.name |  while read pod_name
+         kubectl get pods -n $namespace --field-selector=status.phase!=Running --no-headers  --selector='!job-name'  -o custom-columns=pod_name:.metadata.name |  while read pod_name
         do
-            echo "Checking this pod $pod_name in this ns $namespace" ; kubectl describe pod $pod_name -n $namespace | egrep 'already attached to node|RUN fsck MANUALLY|watch channel had been closed|failed to attach to node|cannot find a network-attachm
-ent-definition'
+            echo "Checking this pod $pod_name in this ns $namespace" ; kubectl describe pod $pod_name -n $namespace | egrep 'already attached to node|RUN fsck MANUALLY|watch channel had been closed|failed to attach to node'
         done
 done
